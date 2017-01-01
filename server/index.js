6047efd74769fef95ba66d5d4761535aedab1042
Nicolas Chaulet <n.chaulet@gmail.com>
2017-01-01 16:46:44 -0500

    Create server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const Geocoder = require('node-geocoder');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/geocode', (req, res, next) => {
+  const geocoder = Geocoder();
+  geocoder.geocode(req.query.address)
+    .then(data => ({
+      raw: data.raw,
+      data
+    }))
+    .then(json => res.send(json))
+    .catch(next);
+});
+
+app.listen(4000);
